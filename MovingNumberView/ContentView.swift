7ee1594bc9c2426233144b3f2c1bc7320199f10d
Wirawit Rueopas <aun.wirawit@gmail.com>
2020-04-12 14:43:32 -0700

    Initial Commit

---
+++ MovingNumberView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovingNumberView
+//
+//  Created by Wirawit Rueopas on 4/12/20.
+//  Copyright © 2020 Wirawit Rueopas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
