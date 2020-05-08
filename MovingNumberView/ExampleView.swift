c2064631d949b0717d99939984e375f90c521bdd
Wirawit Rueopas <aun.wirawit@gmail.com>
2020-05-08 00:40:49 -0700

    Fix decimal places bug, add new example

---
+++ MovingNumberView/ExampleView.swift
@@ -0,0 +1,85 @@
+//
+//  ExampleView.swift
+//  MovingNumberView
+//
+//  Created by Wirawit Rueopas on 5/7/20.
+//  Copyright © 2020 Wirawit Rueopas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExampleView: View {
+    
+    @State private var number = 40.039
+    @State private var decimalPlaces = 2
+    
+    private let presets: [Double] = [
+        0.0099,
+        0.099,
+        9,
+        99,
+        319,
+        999,
+        1_234_567
+    ]
+    
+    var body: some View {
+        VStack {
+            Form {
+                VStack {
+                    MovingNumbersView(
+                        number: number,
+                        numberOfDecimalPlaces: decimalPlaces,
+                        fixedWidth: 300) { str in
+                            Text(str)
+                                .font(.largeTitle)
+                                .fontWeight(.heavy)
+                    }
+                }
+                
+                Section {
+                    Text("\(number)")
+                    Slider(value: $number, in: (0...100_000))
+                    Stepper("Decimal: \(decimalPlaces)", value: $decimalPlaces, in: (0...10))
+                }
+                
+                Section(header: Text("Presets")) {
+                    presetView()
+                        .buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+    }
+    
+    private func presetView() -> some View {
+        let numRows = Int(ceil(Double(presets.count) / 3.0))
+        return ForEach(0..<numRows) { row in
+            self.presetRow(row)
+        }
+    }
+    
+    private func presetRow(_ row: Int) -> some View {
+        let count: Int = min(self.presets.count - row * 3, 3)
+        return HStack(spacing: 8) {
+            ForEach(0..<count) { col in
+                self.presetButton(row, col: col)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(Color.blue)
+            }
+        }
+    }
+    
+    private func presetButton(_ row: Int, col: Int) -> some View {
+        Button(action: {
+            self.number = self.presets[row * 3 + col]
+        }) {
+            Text("\(self.presets[row * 3 + col])")
+        }
+    }
+}
+
+struct ExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExampleView()
+    }
+}
--- MovingNumberView/MovingNumbersView.swift
+++ MovingNumberView/MovingNumbersView.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct MovingNumbersView: View {
+struct MovingNumbersView<Element: View>: View {
     /// The number to show.
     let number: Double
     
@@ -26,7 +26,7 @@ struct MovingNumbersView: View {
     var fixedWidth: CGFloat? = nil
     
     /// Function to build digit, comma, and dot components
-    let elementBuilder: (String) -> Text
+    let elementBuilder: (String) -> Element
     
     var digitStackAnimation: Animation {
         Animation
@@ -58,7 +58,7 @@ struct MovingNumbersView: View {
         // Prepend with 0s that're gone when fraction is 0.
         let numberOfAdditionalZeros = numberOfDecimalPlaces - fractionDigits.count
         if numberOfAdditionalZeros > 0 {
-            fractionDigits = repeatElement(0, count: numberOfAdditionalZeros) + fractionDigits
+            fractionDigits.append(contentsOf: repeatElement(0, count: numberOfAdditionalZeros))
         }
         
         // Work from least significant: [4, 5]
@@ -108,6 +108,7 @@ struct MovingNumbersView: View {
         }
         .frame(width: fixedWidth, alignment: .leading)
         
+        // Debugging
         //        print("All:", allElements)
         //        print("Ids:", allElements.map { $0.id })
         
@@ -122,7 +123,7 @@ struct MovingNumbersView: View {
         // TODO: Is there a better way to calculate layout correctly?
         // Would `PreferenceKey` works?
         let estimatedView = HStack(spacing: 0) {
-            ForEach(allElements) { el -> Text in
+            ForEach(allElements) { el -> Element in
                 switch el {
                 case .minus:
                     return self.elementBuilder("-")
@@ -173,18 +174,129 @@ func getAllDigitsInAscendingSignificance(number: Int) -> [Int] {
     return digits
 }
 
-struct MovingNumbersView_Previews: PreviewProvider {
-    static var previews: some View {
-        VStack {
-            Text("Demo:")
-            MovingNumbersView(
-                number: 123.456,
-                numberOfDecimalPlaces: 3,
-                verticalDigitSpacing: 6) { s in
-                    Text(s)
-                        .font(.headline)
-                        .bold()
+extension MovingNumbersView {
+    enum VisualElementType: CustomDebugStringConvertible, Identifiable {
+        case digit(Int, position: Int)
+        case comma(position: Int)
+        case dot
+        case decimalDigit(Int, position: Int)
+        case minus
+        
+        var id: Int {
+            // - Digit has id as a multiple of 10s,
+            // - Decimal place has a negative multiple of 10s
+            // - 0 is reserved for dot.
+            // - 1 is reserved for minus
+            // - comma is the id of previous digit + 5.
+            //
+            // I.e. -1,234.56 -> [1(minus), 40, 35, 30, 20, 10, 0(dot), -10, -20]
+            switch self {
+            case let .digit(_, position):
+                assert(position > 0)
+                return 10 * position
+            case let .comma(position):
+                assert(position > 0)
+                // Give comma the id between two digits.
+                return 10 * position + 5
+            case let .decimalDigit(_, position):
+                assert(position > 0)
+                return -10 * position
+            case .dot:
+                // Reserve 0 for dot
+                return 0
+            case .minus:
+                // Reserve of "-"
+                return 1
             }
-        }.previewLayout(.fixed(width: 300, height: 300))
+        }
+        var debugDescription: String {
+            switch self {
+            case .dot: return "."
+            case let .comma(p): return ",(pos:\(p))"
+            case let .digit(val, p): return "\(val)(pos:\(p))"
+            case let .decimalDigit(val, p): return "\(val)(pos:\(p))"
+            case .minus: return "-"
+            }
+        }
     }
+    
+    func viewFromElement(_ element: VisualElementType) -> some View {
+        switch element {
+        case let .digit(value, _):
+            return AnyView(self.buildDigitStack(showingDigit: value))
+        case let .decimalDigit(value, _):
+            return AnyView(self.buildDigitStack(showingDigit: value))
+        case .dot:
+            return AnyView(self.buildDot())
+        case .comma:
+            return AnyView(self.buildComma())
+        case .minus:
+            return AnyView(self.buildMinus())
+        }
+    }
+    
+    func buildDigitStack(showingDigit digit: Int) -> some View {
+        let digit = CGFloat(digit)
+        let ds = TenDigitStack(
+            spacing: verticalDigitSpacing,
+            elementBuilder: elementBuilder)
+            .modifier(VerticalShift(
+                diffNumber: digit,
+                digitSpacing: verticalDigitSpacing))
+//            .drawingGroup()
+        return ds
+    }
+    
+    func buildComma() -> some View {
+        elementBuilder(",")
+    }
+    
+    func buildDot() -> some View {
+        elementBuilder(".")
+    }
+    
+    func buildMinus() -> some View {
+        elementBuilder("-")
+    }
+    
+    /// A vertical stack of 9 -> 0.
+    struct TenDigitStack: View {
+        var spacing: CGFloat? = nil
+        let elementBuilder: (String) -> Element
+        var body: some View {
+            VStack(alignment: .leading, spacing: spacing) {
+                ForEach((0...9).reversed(), id: \.self) { iDigit in
+                    self.elementBuilder("\(iDigit)")
+                }
+            }
+            .padding(.bottom, spacing)
+            // Padding so the bottom most digit (0)
+            // has the padding like others.
+        }
+    }
+    
+    struct VerticalShift: GeometryEffect {
+        var diffNumber: CGFloat
+        let digitSpacing: CGFloat
+        
+        var animatableData: CGFloat {
+            get { diffNumber }
+            set { diffNumber = newValue }
+        }
+        
+        func effectValue(size: CGSize) -> ProjectionTransform {
+            // The 0.5 is to center right at a single number.
+            // i.e. for 10 digits the center is between some two numbers.
+            let translationY = -size.height/2 + (size.height / 10) * (diffNumber + 0.5) + digitSpacing/2
+            return .init(.init(
+                translationX: 0,
+                y: translationY
+            ))
+        }
+    }
+}
+
+func round(_ number: Double, numPlaces: Int) -> Double {
+    let power = pow(10.0, Double(numPlaces))
+    return round(number * power)/power
 }
